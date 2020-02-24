d9702af9ece6eae66db7c2a323abf2f9012fc554
orsk-moscow <klekisha@student.21-school.com>
2020-02-24 11:23:44 +0300

    some changes for push_swap proj

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: klekisha <klekisha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: u18188899 <u18188899@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/23 20:49:11 by klekisha          #+#    #+#             */
-/*   Updated: 2019/05/05 17:45:57 by klekisha         ###   ########.fr       */
+/*   Updated: 2020/02/17 18:29:42 by u18188899        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -1466,7 +1466,9 @@ t_list								*ft_lstnew(void const *content,
 size_t content_size);
 
 // Description Allocates (with malloc(3)) and returns a “fresh” link. The
-// variables content and content_size of the new link are initialized by copy of the parameters of the function. If the parameter content is nul, the variable content is initialized to
+// variables content and content_size of the new link are initialized 
+// by copy of the parameters of the function. 
+// If the parameter content is nul, the variable content is initialized to
 // NULL and the variable content_size is initialized to 0 even
 // if the parameter content_size isn’t. The variable next is
 // initialized to NULL. If the allocation fails, the function returns
